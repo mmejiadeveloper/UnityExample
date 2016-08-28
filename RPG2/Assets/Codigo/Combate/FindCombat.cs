83722d21760e9765edb12e47cee0138897439776
Administrator\Administrador <lanh03@hotmail.com>
2016-08-28 12:49:39 -0500

    Projeto rpg
    drescripticoairhjwe dqjeewjqkeq

---
+++ RPG2/Assets/Codigo/Combate/FindCombat.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindCombat : MonoBehaviour
+{
+    public Rigidbody2D _Player_Rigidbody;
+    public GameObject _Canvas_Combate;
+    public float frecuencia_de_combate;
+    public float timer;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if(_Player_Rigidbody.velocity!=Vector2.zero)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer>= frecuencia_de_combate)
+        {
+            print("Entrar en combate!");
+            _Canvas_Combate.SetActive(true);
+            _Player_Rigidbody.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ RPG2/Assets/Codigo/Controlador_Personaje.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlador_Personaje : MonoBehaviour {
+    Rigidbody2D rb;
+    Animator anm;
+    SpriteRenderer sp;
+
+    private float mov_en_x;
+    private float mov_en_y;
+    public float velocidad;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anm = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    void Config()
+    {
+        mov_en_x = Input.GetAxis("Horizontal");
+        mov_en_y = Input.GetAxis("Vertical");
+
+        anm.SetFloat("move", mov_en_y);
+        anm.SetFloat("movex", mov_en_x);
+
+        if (mov_en_x < 0)
+        {
+            
+            anm.SetBool("movl", true);
+
+        }
+        else
+        {
+            anm.SetBool("movl", false);
+        }
+
+    }
+
+    void Update () {
+        Config();
+        rb.velocity = new Vector2(mov_en_x*velocidad,mov_en_y*velocidad);
+    }
+}
