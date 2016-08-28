a5dc0f30b57715e8064f8bd92791b4c32ab1dc82
Administrator\Administrador <lanh03@hotmail.com>
2016-08-28 13:26:16 -0500

    V5

--- RPG2/Assets/Codigo/NewBehaviourScript.cs
+++ RPG2/Assets/Codigo/NewBehaviourScript.cs
@@ -6,6 +6,8 @@ public class NewBehaviourScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("Estamos probando un repositorio de GitHub");
+        print("Estamos probando otro vez los cambios");
+
 	}
 	
 	// Update is called once per frame
